66c0b536d52faaad185eb30935647b64263e4bc7
K1ose <klose@jk404.cn>
2024-12-05 14:44:37 +0800

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // 使 Vite 服务监听所有的网络接口
+    port: 5173         // 确保端口设置正确
+  }
 })
